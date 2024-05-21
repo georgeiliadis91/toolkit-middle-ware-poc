ce26a50da55de1e5ebd01f6be1cb774694efe7de
George Iliadis <georgeiliadis@192.168.1.21>
2024-05-21 16:15:39 +0300

    added instructions

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,24 @@ import EventSubscriber from "./EventSubscriber";
 const App: React.FC = () => {
   const [displayUsers, setDisplayUsers] = useState(true);
   return (
-    <div>
-      <h1>Posts</h1>
+    <section>
+      <ul>
+        <li>
+          When you click on a user we start a request for the post of that user,
+          and register the polling mechanism.
+        </li>
+        <li>
+          Every time you swap the user, we cancel the previous polling mechanism
+          and start a new one, for the new user.
+        </li>
+        <li>When you toggle off the menu, we stop the polling mechanism.</li>
+      </ul>
       <button onClick={() => setDisplayUsers(!displayUsers)}>
         Toggle Users
       </button>
       {displayUsers && <UsersList />}
       <EventSubscriber />
-    </div>
+    </section>
   );
 };
 
